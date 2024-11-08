392f3ab03bd60fd897fd24dee7453edd5faabddc
Yeluzii <2317413623@qq.com>
2024-11-08 12:34:15 +0800

    后台多模块系统搭建

---
+++ base-web/src/main/java/fun/ychen/config/mybatis/MyBatisPlusConfig.java
@@ -0,0 +1,26 @@
+package fun.ychen.config.mybatis;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author dfysa
+ * @data 2024/11/7 下午6:24
+ * @description
+ */
+@Configuration
+@MapperScan("fun.ychen.*.*.mapper")
+public class MyBatisPlusConfig {
+    // 最新版
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new
+                PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
