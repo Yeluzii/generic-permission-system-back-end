522378ccca53d3cc1a3de00b4b87535840644e0c
Yeluzii <2317413623@qq.com>
2024-11-09 16:44:22 +0800

    用户管理模块接口开发

---
+++ base-web/src/main/java/fun/ychen/web/sys_user/service/SysUserService.java
@@ -0,0 +1,7 @@
+package fun.ychen.web.sys_user.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import fun.ychen.web.sys_user.entity.SysUser;
+
+public interface SysUserService extends IService<SysUser> {
+}
